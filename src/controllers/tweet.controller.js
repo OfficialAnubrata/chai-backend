226aa3387848630cc684f23efca08a7054eee6a7
Anubrata Sarkar <anubrata.sarkar@iemlabs.com>
2024-07-26 17:11:53 +0530

    tweet controller complete

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -36,6 +36,10 @@ const getUserTweets = asyncHandler(async (req, res) => {
     if (!isValidObjectId(userId)) {
       throw new ApiError(400, "Invalid user ID");
     }
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new ApiError(404, "User not found");
+    }
     const tweets = await Tweet.find({ owner: userId });
     return res
       .status(200)
@@ -47,10 +51,40 @@ const getUserTweets = asyncHandler(async (req, res) => {
 
 const updateTweet = asyncHandler(async (req, res) => {
   //TODO: update tweet
+  try {
+    const { content } = req.body;
+    if (!content) {
+      throw new ApiError(400, "Content is required");
+    }
+    const tweet = await Tweet.findByIdAndUpdate(
+        req.params.tweetId,
+      { content },
+      { new: true }
+    );
+    if (!tweet) {
+      throw new ApiError(404, "Tweet not found");
+    }
+    return res
+     .status(200)
+     .json(new ApiResponse(200, tweet, "Tweet updated"));
+  } catch (error) {
+    res.json({ error: error.message });    
+  }
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
   //TODO: delete tweet
+  try {
+    const tweet = await Tweet.findByIdAndDelete(req.params.tweetId);
+    if (!tweet) {
+      throw new ApiError(404, "Tweet not found");
+    }
+    return res
+     .status(200)
+     .json(new ApiResponse(200, null, "Tweet deleted"));
+  } catch (error) {
+    res.json({ error: error.message });    
+  }
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
